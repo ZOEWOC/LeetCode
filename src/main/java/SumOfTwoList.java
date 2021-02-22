eea80579fe446ebce3450ab43a9f1cbe011a19ff
zoewoc <zoewoc@outlook.com>
2021-02-22 22:54:26 +0800

    home commit

---
+++ src/main/java/SumOfTwoList.java
@@ -0,0 +1,34 @@
+public class SumOfTwoList {
+
+
+    //     * Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode list = null;
+        while (l1.next != null || l2.next != null) {
+            int num = 0;
+            if (l1.next != null && l2.next != null) {
+
+            }
+        }
+
+        return null;
+
+    }
+}
